aaea4ede14189fac38bcecaa75df7579c60e1fb4
codis <costin.birlea@gmail.com>
2017-04-25 20:47:05 +0300

    Starting to take shape

--- MyBot.cpp
+++ MyBot.cpp
@@ -7,22 +7,80 @@
 #include <fstream>
 
 #include <queue>
-#include <climits>
+#include <list>
+
+//for INT_MAX or INT_MIN
+//#include <climits>
 
 #include "hlt.hpp"
 #include "networking.hpp"
 
-struct LocationScore {
+#include "expansion.h"
+#include "tunneling.h"
 
-    unsigned short x;
-    unsigned short y;
-    int score;
+unsigned char sendBotTo(hlt::Location botLocation, hlt::Location destinationLocation, hlt::GameMap &presentMap) {
 
-};
+    int distance;
+    int minDistance = presentMap.getDistance(presentMap.getLocation(botLocation, 1), destinationLocation);
+    unsigned char currentMove = 1;
 
-static bool operator<(const LocationScore& lsc1, const LocationScore& lsc2) {
+    for(unsigned char i = 2; i < 5; i++) {
+        
+        distance = presentMap.getDistance(presentMap.getLocation(botLocation, i), destinationLocation);
 
-    return (lsc1.score < lsc2.score);
+        if(minDistance > distance) {
+
+            minDistance = distance;
+            currentMove = i;
+
+        }
+
+    }
+
+    return currentMove;
+
+}
+
+void tunneling(hlt::GameMap &presentMap, hlt::Location zone, std::set<hlt::Move> &moves, unsigned char myID) {
+
+    unsigned char dir;
+
+    for(unsigned short a = 0; a < presentMap.height; a++) {
+        
+        for(unsigned short b = 0; b < presentMap.width; b++) {
+            
+            const hlt::Site currentSite = presentMap.getSite({b, a});
+
+            if(currentSite.owner == myID) {
+                
+                //TODO
+                //dont forget to give certain location
+                dir = sendBotTo({ b, a }, {0, 0}, presentMap);
+                const hlt::Site directionSite = presentMap.getSite({b, a}, dir);
+
+                if(myID != directionSite.owner) {
+
+                    if(currentSite.strength >= directionSite.strength) {
+
+                        moves.insert({ { b, a} , dir});                    
+
+                    }
+
+                } else {
+
+                    if(currentSite.strength > 5 * currentSite.production) {
+
+                        moves.insert({ { b, a} , dir});
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
 
 }
 
@@ -31,8 +89,8 @@ unsigned char reverseCardinal( unsigned short cardinal) {
     switch(cardinal) {
 
         case NORTH: return SOUTH;
-        case EAST:  return WEST;
         case SOUTH: return NORTH;
+        case EAST:  return WEST;
         case WEST:  return EAST;
         default: return STILL;
 
@@ -49,191 +107,102 @@ int main() {
     hlt::GameMap presentMap;
     getInit(myID, presentMap);
 
-    //din acest moment avem 15 secunde pana sa facem sendInit, altfel Timeout
-    std::priority_queue<LocationScore> heap;
-    std::ofstream output("Score.txt");
+    int scoreMap[100][100];
+    int acc = 1;
+    int accp = 10;
+    for(unsigned short a = 0; a < presentMap.height; a++) {
+
+        for(unsigned short b = 0; b < presentMap.width; b++) {
+
+            const hlt::Site site = presentMap.getSite({ b, a });
+
+            if(site.strength < 50) {
+                acc = 1;
+            } else if(site.strength < 100) {
+                acc = 3;
+            } else if(site.strength < 150) {
+                acc = 4;
+            }
+
+            if(site.production < 5) {
+                accp = 5;
+            } else if(site.production < 10) {
+                accp = 10;
+            } else if(site.production < 15) {
+                accp = 30;
+            }   
+
+            scoreMap[a][b] = accp * site.production - acc * site.strength;
+
+        }
+
+    }
+ 
+    //Debug file
+    std::ofstream output("Tunnel.txt");
+    output << "ScoreMap\n";
+    for(unsigned short a = 0; a < presentMap.height; a++) {
+
+        for(unsigned short b = 0; b < presentMap.width; b++) {
+
+            output << scoreMap[a][b] << " ";
+
+        }
+
+        output << '\n';
+    }
+
+    output << '\n';
+
+    unsigned short maxLeft, maxRight, maxUp, maxDown, middleI, middleJ;
+
+    int maxSum = matrixSum(presentMap.height, presentMap.width, scoreMap, maxLeft, 
+                            maxRight, maxUp, maxDown);
+
+    // coordonatele mijlocului zonei productive
+    middleI = (maxLeft + maxRight) / 2;
+    middleJ = (maxUp + maxDown) / 2;
+
+    pair<unsigned short, unsigned short> coord = initialPosition(presentMap, myID);
+
+    output << "poz initiala: " << coord.first << " " << coord.second << "\n";
+    output << '\n'; 
+    output << "mijlocul " << middleI << " " << middleJ << "\n";
+    output << maxLeft << " " << maxRight << " " << maxUp << " " << maxDown << "\n";
+    output.close();
 
     sendInit("MyC++Bot");
-//std::vector< std::vector<LocationScore> > scoreMap(presentMap.height, std::vector<LocationScore>(presentMap.width));
-//int scoreMap[presentMap.height][presentMap.width] = {0};
-int acc = 0;
+
     std::set<hlt::Move> moves;
+    unsigned char strategy = 2;
+
     while(true) {
-        moves.clear();acc++;
+        moves.clear();
 
         getFrame(presentMap);
-        //sleep(1); //cam sleep(1) + delta, timp de executie pentru fiecare tura
-        int scoreMap[presentMap.height][presentMap.width] = {0};
 
-        for(unsigned short a = 0; a < presentMap.height; a++) {
-            for(unsigned short b = 0; b < presentMap.width; b++) {
-                
-                if (presentMap.getSite({ b, a }).owner == myID) {
-                    
-                    for(unsigned char i = 1; i < 5; i++) {
-                       
-                        const hlt::Site neighborSite = presentMap.getSite({ b, a }, i);
+        switch(strategy) {
 
-                         if(neighborSite.owner != myID) {
-                           
-                            const hlt::Location neighborLocation = presentMap.getLocation({ b, a }, i);
-                            scoreMap[neighborLocation.y][neighborLocation.x] = neighborSite.production * 5 - (neighborSite.strength * 7 / 10);
-                            heap.push({neighborLocation.x, neighborLocation.y, scoreMap[neighborLocation.y][neighborLocation.x]});
-                            //output << "border( " << neighborLocation.x << ", " << neighborLocation.y << "): " <<  scoreMap[neighborLocation.x][neighborLocation.y] << " ";
-                            //output << "border( " << b << ", " << a << "): " <<  scoreMap[neighborLocation.y][neighborLocation.x] << " ";
-                         }
+            //Tunneling
+            case 1: {
 
-                    }
+                //Tunneling
 
-                }
-                output << scoreMap[a][b] << " ";
+                //if destinatian reached, switch to expansion strategy
+                //if()
+                break;
 
             }
-            output << '\n';
-        }
-        output << '\n'; 
 
-        //grija la indexare aici
-        while(!heap.empty()) {
+            //Expansion
+            case 2: {
 
-            LocationScore site = heap.top();
-            heap.pop();
-            output << "border( " << site.x << ", " << site.y << "): " <<  site.score << " ";
-            for(unsigned char i = 1; i < 5; i++) {
-                
-                const hlt::Site neighborSite = presentMap.getSite({ site.x, site.y}, i);
-                const hlt::Location neighborLocation = presentMap.getLocation({ site.x, site.y}, i);
-                
-                if((neighborSite.owner == myID) && (scoreMap[neighborLocation.y][neighborLocation.x] == 0)) {
-
-                    scoreMap[neighborLocation.y][neighborLocation.x] = site.score - neighborSite.production - 2;
-                    heap.push({neighborLocation.x, neighborLocation.y, scoreMap[neighborLocation.y][neighborLocation.x]});
-
-                }
+                expansionStrategy(presentMap, moves, myID);
+                break;
 
             }
 
         }
-
-        for(unsigned short a = 0; a < presentMap.height; a++) {
-            for(unsigned short b = 0; b < presentMap.width; b++) {
-
-                if (presentMap.getSite({ b, a }).owner == myID) {
-                    
-                    const hlt::Site currentSite = presentMap.getSite({ b, a });
-
-                    if(currentSite.strength > 5 * currentSite.production) {
-
-                        int maxScore = INT_MIN;
-                        unsigned char dir = 0;
-
-                        for(unsigned char i = 1; i < 5; i++) {
-                           
-                            const hlt::Location neighborLocation = presentMap.getLocation({ b, a}, i);
-                            if(maxScore < scoreMap[neighborLocation.y][neighborLocation.x]) {
-
-                                maxScore = scoreMap[neighborLocation.y][neighborLocation.x];
-                                dir = i;
-
-                            }
-
-                        }
-
-                        moves.insert({ { b, a} , dir});
-
-                    }
-
-                }
-
-            }
-        }
-
-        output << std::endl;
-        output << "FRAME" <<acc;
-        for(hlt::Move print : moves)
-
-            output << print.dir + 41 << " ";
-        output << std::endl;
-        output << std::endl;
-        output << std::endl;
-/*
-        for(unsigned short a = 0; a < presentMap.height; a++) {
-            for(unsigned short b = 0; b < presentMap.width; b++) {
-                
-                hlt::Site site = presentMap.getSite({ b, a});
-                if(site.owner == myID) {
-                    /*
-                    int maxScore = INT_MIN;
-                    for(unsigned char i = 1; i < 5; i++) {
-
-                        hlt::Site neighborSite = presentMap.getSite({ b, a}, i);
-                        hlt::Location neighborLocation = presentMap.getLocation({ b, a}, i);
-                        if(maxScore < scoreMap[neighborLocation.y][neighborLocation.x].score) {
-
-                            maxScore = scoreMap[neighborLocation.y][neighborLocation.x].score;
-
-                        }
-
-                    }
-                    //redundant?
-                    if(site.strength == 0) {
-
-                        moves.insert({ {b, a}, 0});
-
-                    } else {
-
-                        if(site.strength < site.production * 5) {
-
-                            moves.insert({ { b, a}, 0});
-
-                        } else {
-
-
-
-                        }
-
-                    }
-                
-                }
-
-            }
-
-        } 
-
-for(unsigned char i = 1; i < 5; i++) {
-                
-                const hlt::Site neighborSite = presentMap.getSite({ site.x, site.y}, i);
-                const hlt::Location neighborLocation = presentMap.getLocation({ site.x, site.y}, i);
-                
-                if((neighborSite.owner == myID) && (scoreMap[neighborLocation.y][neighborLocation.x] == 0)) {
-
-                    scoreMap[neighborLocation.y][neighborLocation.x] = site.score - neighborSite.production - 2;
-
-                    if(neighborSite.strength < neighborSite.production * 5) {
-
-                        moves.insert({ { neighborLocation.x, neighborLocation.y} , 0});
-
-                    }
-
-                    else if((neighborSite.strength < presentMap.getSite({ site.x, site.y}).strength) && (presentMap.getSite({ site.x, site.y}).owner == 0)) {
-
-                            moves.insert({ { neighborLocation.x, neighborLocation.y} , 0});
-
-                            } 
-
-                            else {
-
-                            moves.insert({ { neighborLocation.x, neighborLocation.y} , reverseCardinal(i)});
-                            output << "DAU MISCAREA "<< site.x << " " << site.y << " " << reverseCardinal(i) + 41 << " " << neighborSite.production + 41 << "\n" ;
-                            heap.push({neighborLocation.x, neighborLocation.y, scoreMap[neighborLocation.y][neighborLocation.x]});
-
-                            }
-
-                    }
-
-            }
-
-        */
 
         sendFrame(moves);
     
